dc39bc7aa939b2f7ddab91d0f47b79581de22dc4
lyuser@foxmail.com <lyuser@foxmail.com>
2020-09-19 17:15:25 +0800

    first commit

---
+++ 1.cmd
@@ -0,0 +1,7 @@
+cd C:\npk2img\imgs\sprite_Interface2_event_OccupyDungeon.NPK
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M master
+git remote add origin https://github.com/LoveOyy/sprite_Interface2_event_OccupyDungeon.NPK.git
+git push -u origin master
